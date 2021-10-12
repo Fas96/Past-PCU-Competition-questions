0dfc7b10eebede49801c2db7a0dfb74b4f92feb7
Fas96 <firibu.anyass@gmail.com>
2021-10-12 16:16:50 +0900

    largest area in histogram with complexity of O(n^2)

---
+++ src/LargestrectangleinHistogram.py
@@ -0,0 +1,15 @@
+def LargestrectangleinHistogram(arr):
+    arr = [8, 2, 6, 9, 7, 2]
+    mx = -1 * 10 ** 10
+    for i in range(len(arr)):
+        for j in range(i, len(arr)):
+            print(arr[i:j + 1])
+            H = min(arr[i:j + 1])
+            W = j - i + 1
+            mx = max(mx, H * W)
+
+    return mx
+
+
+if __name__ == '__main__':
+    print(LargestrectangleinHistogram([]))
